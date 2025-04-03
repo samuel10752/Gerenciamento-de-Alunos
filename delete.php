84a155c9777cded30ca08a599506977b8ced6dc5
samuel107152 <samuelcauabarbosa114@gmail.com>
2025-04-03 13:50:29 -0300

    V 1.2 - foi removido ou remanejado para outra pasta

--- delete.php
+++
@@ -1,8 +0,0 @@
-<?php require 'alunosController.php'; ?>
-<?php
-if (isset($_POST['delete_id'])) {
-    deletarAluno($conn, $_POST['delete_id']);
-    header("Location: index.php");
-    exit;
-}
-?>
